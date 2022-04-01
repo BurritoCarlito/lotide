37398a171f4ed926091dc6c4a3850d4c9bc2021d
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-01 21:13:12 +0000

    eslint middle.js

--- middle.js
+++ middle.js
@@ -16,20 +16,18 @@ const assertArraysEqual = function(firstArray, secondArray) {
 };
 
 const middle = function(originalArray) {
-   let midArray = [];
-   let middleIndex = Math.floor(originalArray.length / 2); // floor ex. if 2.5 -> 2 
-   if (originalArray.length <= 2) {
+  let midArray = [];
+  let middleIndex = Math.floor(originalArray.length / 2);// floor ex. if 2.5 -> 2
+  if (originalArray.length <= 2) {
     return midArray;
-  } 
-    else if (originalArray.length % 2 === 0) {
-     midArray.push(originalArray[middleIndex - 1]);
-     midArray.push(originalArray[middleIndex]);
-     return midArray;
-    } 
-    else {
-      midArray.push(originalArray[middleIndex]);
-      return midArray; 
-    }
+  } else if (originalArray.length % 2 === 0) {
+    midArray.push(originalArray[middleIndex - 1]);
+    midArray.push(originalArray[middleIndex]);
+    return midArray;
+  } else {
+    midArray.push(originalArray[middleIndex]);
+    return midArray;
+  }
 };
 
 
