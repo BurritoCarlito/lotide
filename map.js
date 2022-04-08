646484a4973e61d27af5fbc32cfff335aeade897
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-08 23:45:16 +0000

    map function

---
+++ map.js
@@ -0,0 +1,42 @@
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i <= firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+/*
+the map function will take in 2 arguments:
+1. an array to map
+2. a callback function
+
+the map function will return a new array based on the results of the call back function
+*/
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
