c9d9f769ff54e035c21aeff8c008809e6b91ccfb
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-09 19:18:08 +0000

    findKey function for lotide

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected)
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// implement a function findKey which takes in ana object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, they it should return undefined.
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return (key);
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
