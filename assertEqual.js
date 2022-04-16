83f4d1153d66060bfe739edf74eba990dda09859
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-16 21:27:08 +0000

    refactored and updated head.js and assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -5,9 +5,5 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("LighthouseLabs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Carlo", "Ian");
-assertEqual("Carlo", "Carlo");
-assertEqual(3, 3);
-assertEqual(2010, 2022);
+
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("LighthouseLabs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Carlo", "Ian");
+assertEqual("Carlo", "Carlo");
+assertEqual("3", 3);
+assertEqual(2010, 2022);
