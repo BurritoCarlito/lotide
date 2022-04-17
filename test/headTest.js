e275cabf1bebce84801add31c9f25cc646f94587
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-17 04:05:51 +0000

    refactored to include mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -10,7 +10,7 @@ describe("#head", () => {
     assert.strictEqual(head(['5']), '5');
   });
 
-  it("should return 5 for [5, 6,7]", () => {
+  it("should return 5 for [5, 6, 7]", () => {
     assert.strictEqual(head([5, 6, 7]), 5);
   });
 
