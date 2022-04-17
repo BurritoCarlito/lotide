6e21cdcf3759dc87e2da49d2b71830d76c5f9753
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-17 00:01:56 +0000

    refactored middle.js and middleTest.js

--- middle.js
+++ middle.js
@@ -31,11 +31,4 @@ const middle = function(originalArray) {
 };
 
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); // => [3, 4]
+module.exports = middle;
